513e8b0744bb49c3fd7517c3a22875da20f50e33
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-04-04 03:07:32 -0700

    Added code to "EventController.cs" that is able to process registration requests, direct users, and can validate input.

--- COMP003B.Assignment3/Controllers/EventController.cs
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -1,6 +1,36 @@
-﻿namespace COMP003B.Assignment3.Controllers
+﻿using COMP003B.Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Controllers
 {
-	public class EventController
+	public class EventController : Controller
 	{
+		[HttpGet("event/register/{eventCode}")]
+		public IActionResult Register(string eventCode)
+		{
+			var model = new EventRegistration
+			{
+				EventCode = eventCode
+			};
+			return View(model);
+		}
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View(new EventRegistration());
+		}
+		[HttpPost]
+		public IActionResult Register(EventRegistration model)
+		{
+			if (ModelState.IsValid)
+			{
+				return RedirectToAction("Success", model);
+			}
+			return View(model);
+		}
+		public IActionResult Success(EventRegistration model)
+		{
+			return View(model);
+		}
 	}
 }
