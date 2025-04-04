06d31148a7192bdfcd314f4c4aa88b330c61a452
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-04-04 11:22:43 -0700

    Added error and display messages to the file "EventRegistrations.cs", giving specialized messages for each text field and not allowing submissions of invalid responses.

--- COMP003B.Assignment3/Models/EventRegistration.cs
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -1,6 +1,27 @@
-﻿namespace COMP003B.Assignment3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
 {
 	public class EventRegistration
 	{
+		[Required(ErrorMessage = "The FullName field is required.")]
+		[MinLength(3, ErrorMessage = "The field FullName must be a string with a minimum length of 3 and a maximum length of 100.")]
+		[Display(Name = "Full Name")]
+		public string FullName { get; set; }
+		[Required(ErrorMessage = "The Email field is required.")]
+		[EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
+		[Display(Name = "Email Address")]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "The EventCode field is required.")]
+		[StringLength(20, ErrorMessage = "The event code cannot be greater than 20 characters.")]
+		[Display(Name = "Event Code")]
+		public string EventCode { get; set; }
+		[Required(ErrorMessage = "The Tickets field is required.")]
+		[Range(1, 10, ErrorMessage = "The field Tickets must be between 1 and 10.")]
+		[Display(Name = "Number of Tickets")]
+		public int Tickets { get; set; }
+
+		[Display(Name = "Referral Code (Optional)")]
+		public string? ReferralCode { get; set; }
 	}
 }
