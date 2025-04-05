218688d0388bbb3cf9704a837ae69d041db3f3eb
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-04-04 21:36:36 -0700

    Changed the names of the text boxes in "EventRegistration.cs" to better match the names selected in the demo of the assignment, and also added code to the file "Success.cshtml" that informs the user that the registration for tickets was successful, along with code that tells the user information they previously inputted.

--- COMP003B.Assignment3/Models/EventRegistration.cs
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -6,22 +6,21 @@ namespace COMP003B.Assignment3.Models
 	{
 		[Required(ErrorMessage = "The FullName field is required.")]
 		[MinLength(3, ErrorMessage = "The field FullName must be a string with a minimum length of 3 and a maximum length of 100.")]
-		[Display(Name = "Full Name")]
+		[Display(Name = "FullName")]
 		public string FullName { get; set; }
 		[Required(ErrorMessage = "The Email field is required.")]
 		[EmailAddress(ErrorMessage = "The Email field is not a valid e-mail address.")]
-		[Display(Name = "Email Address")]
+		[Display(Name = "Email")]
 		public string Email { get; set; }
 		[Required(ErrorMessage = "The EventCode field is required.")]
 		[StringLength(20, ErrorMessage = "The event code cannot be greater than 20 characters.")]
-		[Display(Name = "Event Code")]
+		[Display(Name = "EventCode")]
 		public string EventCode { get; set; }
 		[Required(ErrorMessage = "The Tickets field is required.")]
 		[Range(1, 10, ErrorMessage = "The field Tickets must be between 1 and 10.")]
-		[Display(Name = "Number of Tickets")]
+		[Display(Name = "Tickets")]
 		public int Tickets { get; set; }
-
-		[Display(Name = "Referral Code (Optional)")]
+		[Display(Name = "ReferralCode")]
 		public string? ReferralCode { get; set; }
 	}
 }
